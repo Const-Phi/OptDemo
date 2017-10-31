f4558739419580866e8c3517ab38c0fe0e6f91d2
Constantine Philipchenko <const-phi@ya.ru>
2017-10-31 12:03:43 +0300

    Rename Range class.

---
+++ OptDemoLib/Range.cs
@@ -0,0 +1,13 @@
+﻿namespace OptDemoLib
+{
+    public struct Range
+    {
+        public double Left;
+
+        public double Right;
+
+        public double GetLength() => Right - Left;
+
+        public override string ToString() => $"[{Left}; {Right}]";
+    }
+}
